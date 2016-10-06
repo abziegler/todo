175646f209444689f833a766fafa547eed91c991
Adam Ziegler <abziegler@gmail.com>
2016-10-05 21:31:10 -0400

    insert todo

---
+++ imports/ui/App.jsx
@@ -0,0 +1,64 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js';
+
+import Task from './Task.jsx';
+
+// App component - represents the whole app
+
+class App extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text field via the REact ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // clear Form
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
+  renderTasks() {
+    return this.props.tasks.map((task) => (
+      <Task key={task._id} task={task} />
+    ));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>To Do</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input 
+              type="text"
+              ref="textInput"
+              placeholder="What do you need to do?"
+            />
+          </form>
+        </header>
+
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  tasks: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+  return {
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+  };
+}, App);
